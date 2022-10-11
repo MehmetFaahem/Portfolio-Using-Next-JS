8cc5b85f4d3c7320284dff514d478c8b46caca40
Mehmet Faahem <mehmetfaahem@gmail.com>
2022-10-11 12:37:03 +0600

    Update and rename hello.js to locations.js

---
+++ pages/api/locations.js
@@ -0,0 +1,647 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+  "locations": [
+    {
+      "state": "ArizonaAS",
+      "cities": [
+        {
+          "slug": "arizona",
+          "title": "Arizona State, AZ"
+        }
+      ]
+    },
+    {
+      "state": "Arizona",
+      "cities": [
+        {
+          "slug": "arizona-state-university",
+          "title": "Arizona State University, AZ"
+        },
+        {
+          "slug": "gilbert-arizona",
+          "title": "Gilbert, AZ"
+        },
+        {
+          "slug": "arizonaphoenix",
+          "title": "Phoenix, AZ"
+        },
+        {
+          "slug": "arizonaqueen-creek",
+          "title": "Queen Creek, AZ"
+        },
+        {
+          "slug": "arizonascottsdale",
+          "title": "Scottsdale, AZ"
+        },
+        {
+          "slug": "arizonatempe",
+          "title": "Tempe, AZ"
+        },
+        {
+          "slug": "tucson-arizona",
+          "title": "Tucson, AZ"
+        }
+      ]
+    },
+    {
+      "state": "California",
+      "cities": [
+        {
+          "slug": "california-irvine",
+          "title": "Irvine, CA"
+        },
+        {
+          "slug": "los-angeles-california",
+          "title": "Los Angeles, CA"
+        },
+        {
+          "slug": "orange-county-california-laundry-delivery-service",
+          "title": "Orange County, CA"
+        },
+        {
+          "slug": "california-san-bernardino",
+          "title": "San Bernardino, CA"
+        },
+        {
+          "slug": "university-irvine",
+          "title": "University Of Irvine, CA"
+        }
+      ]
+    },
+    {
+      "state": "Colorado",
+      "cities": [
+        {
+          "slug": "colorado-aurora",
+          "title": "Aurora, CO"
+        },
+        {
+          "slug": "colorado-denver",
+          "title": "Denver, CO"
+        },
+        {
+          "slug": "coloradolittleton",
+          "title": "Littleton, CO"
+        }
+      ]
+    },
+    {
+      "state": "District of Columbia",
+      "cities": [
+        {
+          "slug": "washington-d-c",
+          "title": "Washington, DC"
+        }
+      ]
+    },
+    {
+      "state": "Florida",
+      "cities": [
+        {
+          "slug": "clearwater-florida-name-laundry-delivery-service",
+          "title": "Clearwater, FL"
+        },
+        {
+          "slug": "fort-lauderdale-florida-laundry-delivery-service",
+          "title": "Fort Lauderdale, FL"
+        },
+        {
+          "slug": "jacksonville-florida-laundry-delivery-service",
+          "title": "Jacksonville, FL"
+        },
+        {
+          "slug": "miami-florida-laundry-service",
+          "title": "Miami, FL"
+        },
+        {
+          "slug": "orlando-florida",
+          "title": "Orlando, FL"
+        },
+        {
+          "slug": "st-petersburg-florida-laundry-delivery-service",
+          "title": "St. Petersburg, FL"
+        },
+        {
+          "slug": "florida-tampa",
+          "title": "Tampa, FL"
+        }
+      ]
+    },
+    {
+      "state": "Georgia",
+      "cities": [
+        {
+          "slug": "acworth",
+          "title": "Acworth, GA"
+        },
+        {
+          "slug": "georgia-alpharetta",
+          "title": "Alpharetta, GA"
+        },
+        {
+          "slug": "atlanta",
+          "title": "Atlanta, GA"
+        },
+        {
+          "slug": "georgia-decatur",
+          "title": "Decatur, GA"
+        },
+        {
+          "slug": "fayetteville-georgia",
+          "title": "Fayetteville, GA"
+        },
+        {
+          "slug": "georgia-tech",
+          "title": "Georgia Tech, GA"
+        }
+      ]
+    },
+    {
+      "state": "Idaho",
+      "cities": [
+        {
+          "slug": "boise-idaho",
+          "title": "Boise, ID"
+        }
+      ]
+    },
+    {
+      "state": "Illinois",
+      "cities": [
+        {
+          "slug": "chicago",
+          "title": "Chicago, IL"
+        },
+        {
+          "slug": "crystal-lake-illinois-laundry-delivery-service",
+          "title": "Crystal Lake, IL"
+        }
+      ]
+    },
+    {
+      "state": "Indiana",
+      "cities": [
+        {
+          "slug": "bloomington-indiana-laundry-service",
+          "title": "Bloomington, IN"
+        },
+        {
+          "slug": "indianapolis",
+          "title": "Indianapolis, IN"
+        }
+      ]
+    },
+    {
+      "state": "Kansas",
+      "cities": [
+        {
+          "slug": "kansas-city-kansas",
+          "title": "Kansas City, KS"
+        },
+        {
+          "slug": "overland-park",
+          "title": "Overland Park, KS"
+        }
+      ]
+    },
+    {
+      "state": "Kentucky",
+      "cities": [
+        {
+          "slug": "lexington-kentucky",
+          "title": "Lexington, KY"
+        },
+        {
+          "slug": "kentucky-louisville",
+          "title": "Louisville, KY"
+        }
+      ]
+    },
+    {
+      "state": "Maryland",
+      "cities": [
+        {
+          "slug": "maryland-baltimore",
+          "title": "Baltimore, MD"
+        },
+        {
+          "slug": "maryland-college-park",
+          "title": "College Park, MD"
+        },
+        {
+          "slug": "maryland-silver-spring",
+          "title": "Silverspring, MD"
+        }
+      ]
+    },
+    {
+      "state": "Massachusetts",
+      "cities": [
+        {
+          "slug": "boston-massachusetts",
+          "title": "Boston, MA"
+        }
+      ]
+    },
+    {
+      "state": "Michigan",
+      "cities": [
+        {
+          "slug": "michiganann-arbor",
+          "title": "Ann Arbor, MI"
+        },
+        {
+          "slug": "detroit-michigan-laundry-delivery-service",
+          "title": "Detroit, MI"
+        },
+        {
+          "slug": "minnesota-laundry-service-near-me",
+          "title": "Minneapolis, MI"
+        },
+        {
+          "slug": "michiganuniversity-of-michigan",
+          "title": "University Of Michigan, MI"
+        },
+        {
+          "slug": "michigan-ypsilanti",
+          "title": "Ypsilanti, MI"
+        }
+      ]
+    },
+    {
+      "state": "Minnesota",
+      "cities": [
+        {
+          "slug": "minneapolis-laundry-service",
+          "title": "Minneapolis-st.paul, MN"
+        },
+        {
+          "slug": "minnesota-st-paul",
+          "title": "Saint Paul, MN"
+        }
+      ]
+    },
+    {
+      "state": "Missouri",
+      "cities": [
+        {
+          "slug": "kansas-city",
+          "title": "Kansas City, MO"
+        },
+        {
+          "slug": "st-louis-missouri",
+          "title": "St. Louis, MO"
+        }
+      ]
+    },
+    {
+      "state": "Nevada",
+      "cities": [
+        {
+          "slug": "nevada-henderson",
+          "title": "Henderson, NV"
+        },
+        {
+          "slug": "las-vegas",
+          "title": "Las Vegas, NV"
+        }
+      ]
+    },
+    {
+      "state": "New Jersey",
+      "cities": [
+        {
+          "slug": "north-brunswick-new-jersey",
+          "title": "North Brunswick, NJ"
+        }
+      ]
+    },
+    {
+      "state": "North Carolina",
+      "cities": [
+        {
+          "slug": "north-carolina-cary",
+          "title": "Cary, NC"
+        },
+        {
+          "slug": "chapel-hill",
+          "title": "Chapel Hill, NC"
+        },
+        {
+          "slug": "charlotte-north-carolina-laundry-delivery-service",
+          "title": "Charlotte, NC"
+        },
+        {
+          "slug": "durham-2",
+          "title": "Durham, NC"
+        },
+        {
+          "slug": "huntersville-north-carolina-laundry-delivery-service",
+          "title": "Huntersville, NC"
+        },
+        {
+          "slug": "north-carolina-raleigh",
+          "title": "Raleigh, NC"
+        },
+        {
+          "slug": "unc-chapel-hill",
+          "title": "Unc At Chapel Hill, NC"
+        }
+      ]
+    },
+    {
+      "state": "Ohio",
+      "cities": [
+        {
+          "slug": "cleveland",
+          "title": "Cleveland, OH"
+        },
+        {
+          "slug": "columbus",
+          "title": "Columbus, OH"
+        },
+        {
+          "slug": "ohio-dayton",
+          "title": "Dayton, OH"
+        },
+        {
+          "slug": "ohio-dublin",
+          "title": "Dublin, OH"
+        },
+        {
+          "slug": "gahanna",
+          "title": "Gahanna, OH"
+        },
+        {
+          "slug": "granville",
+          "title": "Granville, OH"
+        },
+        {
+          "slug": "kenyon-college",
+          "title": "Kenyon College, OH"
+        },
+        {
+          "slug": "ohiomount-vernon",
+          "title": "Mount Vernon, OH"
+        },
+        {
+          "slug": "ohio-parma",
+          "title": "Parma, OH"
+        },
+        {
+          "slug": "strongsville",
+          "title": "Strongsville, OH"
+        },
+        {
+          "slug": "ohio-state-university",
+          "title": "The Ohio State University, OH"
+        },
+        {
+          "slug": "westerville",
+          "title": "Westerville, OH"
+        }
+      ]
+    },
+    {
+      "state": "Oklahoma",
+      "cities": [
+        {
+          "slug": "oklahoma-city-oklahoma-laundry-delivery-service",
+          "title": "Oklahoma City, OK"
+        }
+      ]
+    },
+    {
+      "state": "Oregon",
+      "cities": [
+        {
+          "slug": "oregonbeaverton",
+          "title": "Beaverton, OR"
+        },
+        {
+          "slug": "portland",
+          "title": "Portland, OR"
+        }
+      ]
+    },
+    {
+      "state": "Pennsylvania",
+      "cities": [
+        {
+          "slug": "pennsylvania-philadelphia",
+          "title": "Philadelphia, PA"
+        },
+        {
+          "slug": "pittsburgh",
+          "title": "Pittsburgh, PA"
+        },
+        {
+          "slug": "university-pittsburgh",
+          "title": "University Of Pittsburgh, PA"
+        }
+      ]
+    },
+    {
+      "state": "South Carolina",
+      "cities": [
+        {
+          "slug": "greenville-south-carolina-laundry-delivery-service",
+          "title": "Greenville, SC"
+        }
+      ]
+    },
+    {
+      "state": "Tennessee",
+      "cities": [
+        {
+          "slug": "brentwood",
+          "title": "Brentwood, TN"
+        },
+        {
+          "slug": "tennessee-franklin",
+          "title": "Franklin, TN"
+        },
+        {
+          "slug": "hermitage",
+          "title": "Hermitage, TN"
+        },
+        {
+          "slug": "tennesseemurfreesboro",
+          "title": "Murfreesboro, TN"
+        },
+        {
+          "slug": "nashville",
+          "title": "Nashville, TN"
+        },
+        {
+          "slug": "tennesse-smyrna",
+          "title": "Smyrna, TN"
+        },
+        {
+          "slug": "vanderbilt-university",
+          "title": "Vanderbilt University, TN"
+        }
+      ]
+    },
+    {
+      "state": "Texas",
+      "cities": [
+        {
+          "slug": "texas-allen",
+          "title": "Allen, TX"
+        },
+        {
+          "slug": "arlington",
+          "title": "Arlington, TX"
+        },
+        {
+          "slug": "texas-austin",
+          "title": "Austin, TX"
+        },
+        {
+          "slug": "texasbedford",
+          "title": "Bedford, TX"
+        },
+        {
+          "slug": "texas-carrollton",
+          "title": "Carrollton, TX"
+        },
+        {
+          "slug": "texas-cedar-park",
+          "title": "Cedar Park, TX"
+        },
+        {
+          "slug": "colleyville",
+          "title": "Colleyville, TX"
+        },
+        {
+          "slug": "dallas",
+          "title": "Dallas, TX"
+        },
+        {
+          "slug": "flowermound",
+          "title": "Flower Mound, TX"
+        },
+        {
+          "slug": "fort-worth",
+          "title": "Fort Worth, TX"
+        },
+        {
+          "slug": "frisco",
+          "title": "Frisco, TX"
+        },
+        {
+          "slug": "texas-houston",
+          "title": "Houston, TX"
+        },
+        {
+          "slug": "texas-keller",
+          "title": "Keller, TX"
+        },
+        {
+          "slug": "texas-lewisville",
+          "title": "Lewisville, TX"
+        },
+        {
+          "slug": "texaslittle-elm",
+          "title": "Little Elm, TX"
+        },
+        {
+          "slug": "texas-pflugerville",
+          "title": "Pflugerville, TX"
+        },
+        {
+          "slug": "plano",
+          "title": "Plano, TX"
+        },
+        {
+          "slug": "texas-rock",
+          "title": "Round Rock, TX"
+        },
+        {
+          "slug": "texas-san-antonio",
+          "title": "San Antonio, TX"
+        },
+        {
+          "slug": "southlake",
+          "title": "Southlake, TX"
+        },
+        {
+          "slug": "texas-taylor",
+          "title": "Taylor, TX"
+        },
+        {
+          "slug": "university-texas-san-antonio",
+          "title": "University Of Texas At San Antonio, TX"
+        }
+      ]
+    },
+    {
+      "state": "Utah",
+      "cities": [
+        {
+          "slug": "salt-lake-city",
+          "title": "Salt Lake City, UT"
+        }
+      ]
+    },
+    {
+      "state": "Virginia",
+      "cities": [
+        {
+          "slug": "virginia-chesapeake",
+          "title": "Chesapeake, VA"
+        },
+        {
+          "slug": "virginia-hyattsville",
+          "title": "Hyattsville, VA"
+        },
+        {
+          "slug": "virginia-norfolk",
+          "title": "Norfolk, VA"
+        },
+        {
+          "slug": "virginia-virginia-beach",
+          "title": "Virginia Beach, VA"
+        }
+      ]
+    },
+    {
+      "state": "Washington",
+      "cities": [
+        {
+          "slug": "washington-bellevue",
+          "title": "Bellevue, WA"
+        },
+        {
+          "slug": "washington-poulsbo",
+          "title": "Poulsbo, WA"
+        },
+        {
+          "slug": "washington-renton",
+          "title": "Renton, WA"
+        },
+        {
+          "slug": "seattle",
+          "title": "Seattle, WA"
+        },
+        {
+          "slug": "vancouver",
+          "title": "Vancouver, WA"
+        }
+      ]
+    },
+    {
+      "state": "Wisconsin",
+      "cities": [
+        {
+          "slug": "madison",
+          "title": "Madison, WI"
+        }
+      ]
+    }
+  ]
+})
+}
